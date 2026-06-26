9fda2109eca083921a18dbdd86a6c3371e2e3dee
Adil Khan <kj3557983@gmail.com>
2026-06-26 11:10:26 +0500

    Create the SQLite database on startup

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Pulse.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
+                      ?? "Data Source=pulse.db"));
+
 var app = builder.Build();
+
+// Code-first: create the SQLite database on first run.
+using (var scope = app.Services.CreateScope())
+{
+    scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.EnsureCreated();
+}
 
 if (!app.Environment.IsDevelopment())
 {
